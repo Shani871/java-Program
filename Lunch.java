43808b1f993ea984c73853c6e3d10cfa8c9daec1
Shani871 <shanichauhan9838@gmail.com>
2024-02-24 11:42:57 +0530

    Dve1

--- Lunch.java
+++ Lunch.java
@@ -2,5 +2,9 @@ public class Lunch{
    public static void main(String[]args)
    {
      System.out.print("Hello");
+     int a = 20;
+     int b = 45;
+     int sum = a+b;
+     System.out.print(sum);
      }
  }
