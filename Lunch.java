4975f8cc444222418b8acba22ff53687ba77e969
Shani871 <shanichauhan9838@gmail.com>
2024-02-24 11:09:20 +0530

    Ential Commit

---
+++ Lunch.java
@@ -0,0 +1,6 @@
+public class Lunch{
+   public static void main(String[]args)
+   {
+     System.out.print("Hello");
+     }
+ }
---
+++ shani.java
@@ -0,0 +1,8 @@
+public class shani{
+
+ public static void main(String []args)
+ {
+ 
+  System.out.println("Git Hub Learn");
+  }
+  }
